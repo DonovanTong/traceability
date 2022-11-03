0cecca58be02de623db829e30443e8571db20056
Donovan Tong <donotong01@gmail.com>
2022-11-03 15:10:20 -0600

    Initial

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+require('dotenv').config()
+
+app.use(express.json())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
